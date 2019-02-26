bfdb85b2ef8a63f46c0fd3ff2e5ff30398633192
karlaprzbr <karla.perezbrugues@gmail.com>
2019-02-26 16:22:41 +0100

    change algo url

--- js/fetchModule.js
+++ js/fetchModule.js
@@ -1,16 +1,18 @@
-function fetchModule(e, url) {
-  e.preventDefault();
+function fetchModule(e, url, title) {
+  e.preventDefault()
   fetch(url)
   .then(function(response) {
     return response.text()
   })
   .then(function(html) {
-    document.querySelector('#algoDisplay').innerHTML = html;
-	document.body.scrollTop = 0;
-	document.documentElement.scrollTop = 0;
-    MathJax.Hub.Queue(["Typeset",MathJax.Hub]);
+    document.querySelector('#algoDisplay').innerHTML = html
+  	document.body.scrollTop = 0
+  	document.documentElement.scrollTop = 0
+    window.history.pushState(null, title, url)
+    document.title = "FREEFEM++ - " + title
+    MathJax.Hub.Queue(["Typeset",MathJax.Hub])
   })
   .catch(function(err) {
-    console.log('Failed to fetch page: ', err);
+    console.log('Failed to fetch page: ', err)
   });
 }
