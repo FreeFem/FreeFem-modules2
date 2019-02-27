43949085b4c1ec1e0dcbb685905d14033dd1f7be
Simon Garnotel <simon.garnotel@gmail.com>
2019-02-27 16:27:18 +0100

    fix fetch

--- js/fetchModule.js
+++ js/fetchModule.js
@@ -1,18 +1,30 @@
 function fetchModule(e, url, title) {
-  e.preventDefault()
-  fetch(url)
-  .then(function(response) {
-    return response.text()
-  })
-  .then(function(html) {
-    document.querySelector('#algoDisplay').innerHTML = html
-  	document.body.scrollTop = 0
-  	document.documentElement.scrollTop = 0
-    window.history.pushState(null, title, url)
-    document.title = "FREEFEM++ - " + title
-    MathJax.Hub.Queue(["Typeset",MathJax.Hub])
-  })
-  .catch(function(err) {
-    console.log('Failed to fetch page: ', err)
-  });
+	e.preventDefault()
+	fetch(url)
+		.then(function(response) {
+			return response.text()
+		})
+		.then(function(html) {
+			// Get content (not surrounding header, footer, nav, ...)
+			var parser = new DOMParser()
+			var content = parser.parseFromString(html, "text/html")
+			var algoDisplay = content.getElementById('algoDisplay')
+
+			// Replace in the page
+			document.querySelector('#algoDisplay').innerHTML = algoDisplay.innerHTML
+
+			// Reset scroll
+			document.body.scrollTop = 0
+			document.documentElement.scrollTop = 0
+
+			// Set history
+			window.history.pushState(null, title, url)
+			document.title = "FREEFEM++ - " + title
+
+			// Relaunch MathJax
+			MathJax.Hub.Queue(["Typeset",MathJax.Hub])
+		})
+		.catch(function(err) {
+			console.log('Failed to fetch page: ', err)
+		})
 }
